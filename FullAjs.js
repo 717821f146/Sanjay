470354d583c62a46c5118dbd7069a4f79be021c8
717821f146 <140424369+717821f146@users.noreply.github.com>
2023-07-24 08:26:37 -0700

    Add files via upload

---
+++ FullAjs.js
@@ -0,0 +1,11 @@
+function validate(){
+	//var email=document.getElementById("Email");
+	var email="Sanjaymajestic143@gmail.com"
+	var valid="@gamil.com$";
+	if(email.match(valid)){
+		alert("Thank you for Visiting")
+	}
+	else{
+		alert("Email is Incorrect");
+	}
+}
